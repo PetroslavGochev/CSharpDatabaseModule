d57af94501add59af64b031be96d19cf7dca77ee
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-02-01 21:40:53 +0200

    Entity Framework Core Module

---
+++ Entity Framework  Core/01.ADO.NET/02.VillianNames/03.MinionNames/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Text;
+
+namespace _03.MinionNames
+{
+    public class StartUp
+    {
+       private const string connectionString = @"Server=.;Database=MinionsDB;Integrated Security = true";
+        static void Main(string[] args)
+        {
+            using SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            Console.WriteLine(GetVillianWithId(sqlConnection, 1));
+            
+        }
+
+        public static string GetVillianWithId(SqlConnection sqlConnection,int villianId)
+        {
+            StringBuilder sb = new StringBuilder();
+            string query = 
+                @"SELECT [Name] FROM Villains
+                  WHERE Id = @villianId";
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@villianId", villianId);
+            string sqlReader = sqlCommand.ExecuteScalar()?.ToString();
+            sb.Append($"Viillian: ");
+            sb.Append(sqlReader);
+            return sb.ToString();
+        } 
+    }
+}
---
+++ Entity Framework  Core/01.ADO.NET/02.VillianNames/ADB.NET/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace ADB.NET
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Server=.;Database=MinionsDB;Integrated Security = true";
+            using SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            string query =
+                    @"SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount
+                    FROM Villains AS V
+                    JOIN MinionsVillains AS mv
+                    ON v.Id = mv.VillainId
+                    GROUP BY v.Id,v.Name
+                    HAVING COUNT (mv.VillainId) > 3
+                    ORDER BY COUNT (mv.VillainId)";
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            SqlDataReader sqlReader = sqlCommand.ExecuteReader();
+            while (sqlReader.Read())
+            {
+                string firstname = sqlReader["Name"]?.ToString();
+                string countOfMinions = sqlReader["MinionsCount"]?.ToString();
+                Console.WriteLine($"{firstname} - {countOfMinions}");
+            }
+
+
+
+        }
+    }
+}
