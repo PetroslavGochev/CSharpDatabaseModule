d864a44f09d60f9a88619477089dea99e970ec08
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-03-05 15:43:37 +0200

    Entity Framework Core Module

--- Entity Framework  Core/10.BEST PRACTICE AND ARCHITECTURE/RealEstates/RealEstates.Web/Controllers/PropertiesController.cs
+++ Entity Framework  Core/10.BEST PRACTICE AND ARCHITECTURE/RealEstates/RealEstates.Web/Controllers/PropertiesController.cs
@@ -1,20 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RealEstates.Data;
 using RealEstates.Services;
+using System.Linq;
 
 namespace RealEstates.Web.Controllers
 {
-    public class PropertiesController : Controller    
+    public class PropertiesController : Controller
     {
         private readonly IPropertiesServices propertyServices;
+        private readonly RealEstateContext db;
 
-        public PropertiesController(IPropertiesServices propertyServices)
+        public PropertiesController(IPropertiesServices propertyServices,RealEstateContext db)
         {
             this.propertyServices = propertyServices;
+            this.db = db;
         }
 
         public IActionResult Search()
         {
-           
+
             return this.View();
         }
 
@@ -28,10 +32,23 @@ namespace RealEstates.Web.Controllers
 
             return this.View();
         }
-        public IActionResult DoSearchBySize(int minYear, int maxYear,int minSize,int maxSize)
+        public IActionResult DoSearchBySize(int minYear, int maxYear, int minSize, int maxSize)
         {
             var properties = this.propertyServices.Search(minYear, maxYear, minSize, maxSize);
             return this.View(properties);
+        }
+
+        public IActionResult AddProperty()
+        {
+            return this.View();
+        }
+        public IActionResult CreateProperty(string district, int size, string buildingType, string propertyType, decimal price, int? year, byte? floor, byte? totalFloor)
+        {
+            this.propertyServices.Create(district, size, buildingType, propertyType, price, year, floor, totalFloor);
+            var property = this.db.RealEstateProperties
+                .OrderBy(x=>x.Id)
+                .Last();
+            return this.View(property);
         }
 
     }
